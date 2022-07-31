73ac66aec5cdb8ae0d60483439761a7ab2096ddb
discegauderem8 <discegaudere113399@gmail.com>
2022-07-31 23:52:21 +0300

    итоговая версия программы

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+string[]initialArray = {"aghja", "ajana", "a", "adidas", "bb"} ;
+
+string[]finalArray = new string[initialArray.Length];
+
+int count = 0;
+
+for (int i = 0; i < initialArray.Length; i++)
+{
+    if (initialArray[i].Length >= 3)
+    {
+        finalArray[count] = initialArray[i];
+        count++;
+    }
+}
+
+for (int k = 0; k < finalArray.Length; k++)
+{
+    {
+        Console.WriteLine(finalArray[k]);
+    }
+}
+
